0319049ea8a007f275d0ed6024ed233abc39a36f
Strenge Manuel <manuel.strenge@zuehlke.com>
2023-04-25 14:26:07 +0200

    added catppucin  as a template for icon themes

--- go-palette/README.md
+++
@@ -1,8 +0,0 @@
-
-
-## Usage
-
-1. `go get github.com/thebromo/bromium`
-2. Add an`import bromium "github.com/thebromo/bromium"` to your file
-3. Use the flavours as you wish.
-
---
+++ vsc-icon-theme/src/associations/base.ts
@@ -0,0 +1,8 @@
+export const base: Base = {
+  hidesExplorerArrows: false,
+  file: 'file',
+  folder: 'folder',
+  folderExpanded: 'folder__open',
+  rootFolder: 'folder_root',
+  rootFolderExpanded: 'folder_root__open',
+}
